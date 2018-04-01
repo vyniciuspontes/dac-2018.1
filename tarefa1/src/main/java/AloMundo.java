70b0618bc01c06413bb7045e00234ce8bec8a5cf
João Melo <joao_melo@id.uff.br>
2018-04-01 14:44:50 -0300

    Criacao de projeto Maven; Conclusao do servlet AloMundo

---
+++ tarefa1/src/main/java/AloMundo.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author gabri
+ */
+public class AloMundo extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AloMundo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<p>Informe seu nome:</p>");
+            out.println("<form action=\" AloMundo \" method=\"post\">");
+            out.println("<input type=\"text\" name=\"nome\"/>");
+            out.println("<input type=\"submit\" value=\"Enviar\"/>");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String nome = request.getParameter("nome");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html><head>");
+        out.println("<title>Servlet AloMundo</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Alo " + nome + "!</p>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
