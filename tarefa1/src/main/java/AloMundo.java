d5696765acb3b6597932fc5af19233702beb7474
Maico Borges <maicoqb@gmail.com>
2018-04-02 01:31:00 -0300

    Limpando código: - Removendo exceção não utilizada, - removendo método não utlizado, - removendo código gerado automaticamente.
    Adicionando pastas de configuração o IntelliJ ao ignore

--- tarefa1/src/main/java/AloMundo.java
+++ tarefa1/src/main/java/AloMundo.java
@@ -6,7 +6,6 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,15 +16,22 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class AloMundo extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AloMundo</title>");            
+            out.println("<title>Servlet AloMundo</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<p>Informe seu nome:</p>");
@@ -40,30 +46,15 @@ public class AloMundo extends HttpServlet {
     }
 
     /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
      * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         
         String nome = request.getParameter("nome");
@@ -80,15 +71,5 @@ public class AloMundo extends HttpServlet {
         out.close();
         
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
