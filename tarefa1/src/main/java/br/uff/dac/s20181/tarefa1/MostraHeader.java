5c44c40807ad639938a047f04744258bd264005b
Maico Borges <maicoqb@gmail.com>
2018-04-02 02:06:50 -0300

    Adicionando funcionalidade de mostrar header.

--- tarefa1/src/main/java/br/uff/dac/s20181/tarefa1/MostraHeader.java
+++ tarefa1/src/main/java/br/uff/dac/s20181/tarefa1/MostraHeader.java
@@ -1,7 +1,41 @@
 package br.uff.dac.s20181.tarefa1;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class MostraHeader extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Mostra Header</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<p>Os Headers utlizados nesta requisição foram: s</p>");
+            out.println("<ul>");
+
+            Enumeration<String> headers = request.getHeaderNames();
+            while (headers.hasMoreElements()) {
+                String headerName = headers.nextElement();
+                String headerValue = request.getHeader(headerName);
+                String listItem = "<li><b>" + headerName + ": </b> " + headerValue + "</li>";
+                out.println(listItem);
+            }
+
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
+        }
+
+    }
 }
